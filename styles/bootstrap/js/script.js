d8baf915e1e4f361ed83030cc673e165f7eb5b7e
MitanOmar <mitan@gmx.ch>
2020-05-24 18:21:34 +0200

    refactore

--- styles/bootstrap/js/script.js
+++ styles/bootstrap/js/script.js
@@ -1,6 +1,7 @@
 function ajaxRemote (method, url, data, successCallback)
 {
     var xhttp = new XMLHttpRequest();
+    let sendData = null;
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             successCallback(this);
@@ -9,7 +10,7 @@ function ajaxRemote (method, url, data, successCallback)
     xhttp.open(method, url, true);
     if (method == 'POST') {
         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        sendData = data;
     }
-    let sendData = method == 'POST' ? data : null
     xhttp.send(sendData);
 }
