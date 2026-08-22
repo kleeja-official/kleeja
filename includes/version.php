304a9f0509a7137ec8205eaf9b01d5e0617a1fa7
Mitan Omar <git@mitan.dev>
2026-08-22 17:39:36 +0200

    refactor: change the minimum PHP version to 8

--- includes/version.php
+++ includes/version.php
@@ -20,5 +20,5 @@ define('KLEEJA_VERSION', '3.2.0');
 define('KLEEJA_DB_VERSION', '9');
 
 // Kleeja min requirements
-define('MIN_PHP_VERSION', '7.0');
+define('MIN_PHP_VERSION', '8.0');
 define('MIN_MYSQL_VERSION', '4.2.2');
